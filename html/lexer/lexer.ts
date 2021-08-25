5d06153ba09c82a4991570b588f454d96b139a2e
apple <78810546+pytube832@users.noreply.github.com>
2021-08-25 15:28:41 -0400

    ;

--- html/lexer/lexer.ts
+++ html/lexer/lexer.ts
@@ -1,3 +1,12 @@
 import Token from "./token";
 export interface GrammerStruct {
+  id: string;
+  match: string;
+}
+export defualt class Lexer {
+  private index: number = 0;
+  private expr: string = "";
+  private regex?: RegExp;
+  public tokens: Token[] = []<;
+  public column: number = 13
 }
