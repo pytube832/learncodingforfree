b1f4a17dfb35c83b733ae28283305c0dd4c98c71
apple <78810546+pytube832@users.noreply.github.com>
2021-08-25 14:21:14 -0400

    Update index.js

--- html/index.js
+++ html/index.js
@@ -1,3 +1,3 @@
 var cookie = document.getElementsByTagName("cookie")[0];
 var c = cookie.getElementsByTagName("enabled")[0];
-if(navigator.cookieEnabled){console.log("thank you for having cookies enabled")}else{cookie.c.innerHTML = "please enable cookies";}
+if(navigator.cookieEnabled){console.log("thank you for having cookies enabled")}else{c.innerHTML = "please enable cookies";}
