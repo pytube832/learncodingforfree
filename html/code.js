e236d1045cb15c0a56a80df318aac671a7f7d35e
apple <78810546+pytube832@users.noreply.github.com>
2021-08-25 14:51:43 -0400

    Update and rename index.js to code.js

---
+++ html/code.js
@@ -0,0 +1,10 @@
+class cookie(){
+  static cookiesenabled(){
+    return navigator.cookieEnabled;
+  }
+} 
+class run(){
+  static start(){
+    import * as Run from '/run/Run.js';
+  }
+}
--- html/index.js
+++
@@ -1,3 +0,0 @@
-var cookie = document.getElementsByTagName("cookie")[0];
-var c = cookie.getElementsByTagName("enabled")[0];
-if(navigator.cookieEnabled){console.log("thank you for having cookies enabled")}else{alert("please enable cookies");}
